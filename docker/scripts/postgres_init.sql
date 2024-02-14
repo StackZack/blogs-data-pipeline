271b464a073f5efebaa78c0b62016a10c362b63b
Zackary Luedtke <luedtkezack@gmail.com>
2024-02-14 16:57:04 +0100

    Added gold tables to postgres init sql. Updated doc images for typos. Changed spark job to tags for first iterations.

--- docker/scripts/postgres_init.sql
+++ docker/scripts/postgres_init.sql
@@ -1,6 +1,7 @@
 CREATE SCHEMA staging;
 CREATE SCHEMA gold;
 
+-- STAGE TABLE CREATION
 CREATE TABLE staging.stg_users (
     user_id TEXT,
     first_name TEXT,
@@ -56,4 +57,49 @@ CREATE TABLE staging.stg_opinions (
     opinion TEXT,
     created_at TEXT,
     updated_at TEXT
+);
+
+-- GOLD TABLE CREATION
+CREATE TABLE gold.tag_lookup (
+    tag_id INT,
+    name VARCHAR(50)
+);
+
+CREATE TABLE gold.blogs (
+    blog_id INT,
+    tag_ids INT ARRAY,
+    title VARCHAR(150),
+    content TEXT
+);
+
+CREATE TABLE gold.users (
+    user_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(150)
+);
+
+CREATE TABLE gold.comment (
+    comment_id INT,
+    content TEXT
+);
+
+CREATE TABLE gold.favorites (
+    favorite_id INT,
+    favorite_date DATE
+);
+
+CREATE TABLE gold.opinions (
+    opinion_id INT,
+    opinion BOOLEAN
+);
+
+CREATE TABLE gold.blog_activity (
+    blog_activity_id INT,
+    blog_id INT,
+    user_id INT,
+    comment_id INT,
+    favorite_id INT,
+    opinion_id INT,
+    tag_id INT
 );
