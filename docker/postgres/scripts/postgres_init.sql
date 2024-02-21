fcd7cddf43d45bb9c5c94ac2fc5b98d1bc7f8ff9
Zackary Luedtke <luedtkezack@gmail.com>
2024-02-21 13:48:23 +0100

    Updated dw erd. Added constraints to datawarehouse tables.

--- docker/postgres/scripts/postgres_init.sql
+++ docker/postgres/scripts/postgres_init.sql
@@ -61,45 +61,59 @@ CREATE TABLE staging.stg_opinions (
 
 -- GOLD TABLE CREATION
 CREATE TABLE gold.tag_lookup (
-    tag_id INT,
+    tag_id INT PRIMARY KEY,
     name VARCHAR(50)
 );
 
 CREATE TABLE gold.blogs (
-    blog_id INT,
+    blog_id INT PRIMARY KEY,
     tag_ids INT ARRAY,
     title VARCHAR(150),
     content TEXT
 );
 
 CREATE TABLE gold.users (
-    user_id INT,
+    user_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(150)
 );
 
-CREATE TABLE gold.comment (
-    comment_id INT,
+CREATE TABLE gold.comments (
+    comment_id INT PRIMARY KEY,
     content TEXT
 );
 
 CREATE TABLE gold.favorites (
-    favorite_id INT,
+    favorite_id INT PRIMARY KEY,
     favorite_date DATE
 );
 
 CREATE TABLE gold.opinions (
-    opinion_id INT,
+    opinion_id INT PRIMARY KEY,
     opinion BOOLEAN
 );
 
 CREATE TABLE gold.blog_activity (
-    blog_activity_id INT,
+    blog_activity_id SERIAL PRIMARY KEY,
     blog_id INT,
     user_id INT,
     comment_id INT,
     favorite_id INT,
     opinion_id INT,
-    tag_id INT
+    CONSTRAINT fk_blog_id
+        FOREIGN KEY (blog_id)
+            REFERENCES gold.blogs(blog_id),
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+            REFERENCES gold.users(user_id),
+    CONSTRAINT fk_comment_id
+        FOREIGN KEY (comment_id)
+            REFERENCES gold.comments(comment_id),
+    CONSTRAINT fk_favorite_id
+        FOREIGN KEY (favorite_id)
+            REFERENCES gold.favorites(favorite_id),
+    CONSTRAINT fk_opinion_id
+        FOREIGN KEY (opinion_id)
+            REFERENCES gold.opinions(opinion_id)
 );
